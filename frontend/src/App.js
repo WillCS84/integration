c28a8391cf315b1d61d02c7c94275d0ed65715d5
Willian.Santos <will.cesar.84@gmail.com>
2021-12-07 10:44:39 -0300

    propriedades React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'; 
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Integração Alexandria</h1>
+    <Header title="Integração Alexandria"/>
   );
 }
 
